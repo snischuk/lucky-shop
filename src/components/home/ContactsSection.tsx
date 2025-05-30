3dd75ed3739aac14973732fea4f49fa81c968645
Snischuk Maxim <m.snischuk@gmail.com>
2025-05-30 04:14:30 +0300

    chore: add local fonts for project

--- src/components/home/ContactsSection.tsx
+++ src/components/home/ContactsSection.tsx
@@ -1,5 +1,5 @@
 const ContactsSection = () => (
-  <section className="bg-white px-4 py-12 h-1">
+  <section className="h-1 bg-white px-4 py-12">
     <h2 className="mb-4 text-2xl font-semibold">Наші переваги</h2>
     <ul className="list-disc space-y-2 pl-6">
       <li className="bg-red-400">Швидкий запуск</li>
