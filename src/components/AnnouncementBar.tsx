91135490c07aab987e9a9bdac61c71adebd33ee6
Mariia <myshyck@gmail.com>
2025-06-04 10:04:46 +0300

    fix: AnnouncementBarProps

--- src/components/AnnouncementBar.tsx
+++ src/components/AnnouncementBar.tsx
@@ -1,11 +1,12 @@
 import Delivery from '../assets/images/Delivery.svg?react';
 import Close from '../assets/images/Close_white.svg?react';
+import type { FC } from 'react';
 
-type AnnouncementBarProps = {
+type Props = {
   onClose: () => void;
 };
 
-const AnnouncementBar = ({ onClose }: AnnouncementBarProps) => {
+const AnnouncementBar: FC<Props> = ({ onClose }) => {
   return (
     <div className="bg-black p-2 text-main">
       <div className="relative mx-auto flex w-full max-w-custom-1440 items-center justify-center gap-4 font-family-secondary">
