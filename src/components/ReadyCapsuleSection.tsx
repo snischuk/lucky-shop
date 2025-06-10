401487bd2c558e3488eab9412370116a2f52c453
halyna <galinabandurka7@gmail.com>
2025-06-10 18:13:23 +0200

    chore: rename line icons to Icon* for consistency

--- src/components/ReadyCapsuleSection.tsx
+++ src/components/ReadyCapsuleSection.tsx
@@ -5,11 +5,11 @@ import man2x from '../assets/images/home/ready-capsule/man@2x.jpg';
 import woman from '../assets/images/home/ready-capsule/woman.jpg';
 import woman2x from '../assets/images/home/ready-capsule/women@2x.jpg';
 import IconDotTag from '../assets/images/icons/dot-tag.svg?react';
-import LinePantsFemale from '../assets/images/icons/line-pants-female.svg?react';
-import LinePantsMale from '../assets/images/icons/line-pants-male.svg?react';
-import LineShirtMale from '../assets/images/icons/line-shirt-male.svg?react';
-import LineTopFemale from '../assets/images/icons/line-top-female.svg?react';
-import LineTshirtMale from '../assets/images/icons/line-tshirt-male.svg?react';
+import IconLinePantsFemale from '../assets/images/icons/line-pants-female.svg?react';
+import IconLinePantsMale from '../assets/images/icons/line-pants-male.svg?react';
+import IconLineShirtMale from '../assets/images/icons/line-shirt-male.svg?react';
+import IconLineTopFemale from '../assets/images/icons/line-top-female.svg?react';
+import IconLineTshirtMale from '../assets/images/icons/line-tshirt-male.svg?react';
 
 const ReadyCapsuleSection: FC = () => {
   return (
@@ -23,12 +23,12 @@ const ReadyCapsuleSection: FC = () => {
           <div className="absolute left-[137px] top-[52px] flex h-8 w-[68px] items-center justify-center border border-light-black px-5 py-2.5 text-light-black">
             Топ
           </div>
-          <LineTopFemale className="absolute left-[205px] top-[68px] h-[59px] w-[46px]" />
+          <IconLineTopFemale className="absolute left-[205px] top-[68px] h-[59px] w-[46px]" />
           <IconDotTag className="absolute left-[245px] top-[125px] h-[26px] w-[26px]" />
           <div className="absolute left-[551px] top-[313px] flex h-8 w-[92px] items-center justify-center border border-light-black px-5 py-2.5 text-light-black">
             Штани
           </div>
-          <LinePantsFemale className="absolute left-[507px] top-[329px] h-[42px] w-[43px]" />
+          <IconLinePantsFemale className="absolute left-[507px] top-[329px] h-[42px] w-[43px]" />
           <IconDotTag className="absolute left-[488px] top-[369px] h-[26px] w-[26px]" />
         </div>
         <div className="relative">
@@ -36,17 +36,17 @@ const ReadyCapsuleSection: FC = () => {
           <div className="absolute left-[406px] top-[72px] flex h-8 w-[117px] items-center justify-center border border-light-black px-5 py-2.5 text-light-black">
             Футболка
           </div>
-          <LineTshirtMale className="absolute left-[296px] top-[88px] h-[143px] w-[110px]" />
+          <IconLineTshirtMale className="absolute left-[296px] top-[88px] h-[143px] w-[110px]" />
           <IconDotTag className="absolute left-[275px] top-[228px] h-[26px] w-[26px]" />
           <div className="absolute left-[490px] top-[151px] flex h-8 w-[108px] items-center justify-center border border-light-black px-5 py-2.5 text-light-black">
             Сорочка
           </div>
-          <LineShirtMale className="absolute left-[447px] top-[167px] h-[42px] w-[43px]" />
+          <IconLineShirtMale className="absolute left-[447px] top-[167px] h-[42px] w-[43px]" />
           <IconDotTag className="absolute left-[426px] top-[206px] h-[26px] w-[26px]" />
           <div className="absolute left-[198px] top-[543px] flex h-8 w-[92px] items-center justify-center border border-light-black px-5 py-2.5 text-light-black">
             Штани
           </div>
-          <LinePantsMale className="absolute left-[290px] top-[516px] h-[42px] w-[43px]" />
+          <IconLinePantsMale className="absolute left-[290px] top-[516px] h-[42px] w-[43px]" />
           <IconDotTag className="absolute left-[320px] top-[490px] h-[26px] w-[26px]" />
         </div>
       </div>
