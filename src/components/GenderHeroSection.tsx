f0d58692f5f3a38bb218223c934d39a3c303e780
Mariia <myshyck@gmail.com>
2025-06-12 16:22:37 +0300

    feat: add hero and category in Gender Page

--- src/components/GenderHeroSection.tsx
+++ src/components/GenderHeroSection.tsx
@@ -1,10 +1,29 @@
 import type { FC } from 'react';
 
-const GenderHeroSection: FC = () => {
+import man from '../assets/images/man/hero.jpg';
+import man2x from '../assets/images/man/hero@2x.jpg';
+import woman from '../assets/images/woman/hero.jpg';
+import woman2x from '../assets/images/woman/hero@2x.jpg';
+import type { Gender } from '../types/Gender';
+
+interface GenderHeroSectionProps {
+  gender: Gender;
+}
+const GenderHeroSection: FC<GenderHeroSectionProps> = ({ gender }) => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-2xl font-bold">Gender Hero Section</h1>
-    </div>
+    <section className="relative mx-auto flex max-w-custom-1440">
+      {gender === 'women' ? (
+        <img src={woman} srcSet={`${woman2x} 2x`} alt="woman"></img>
+      ) : (
+        <img src={man} srcSet={`${man2x} 2x`} alt="man"></img>
+      )}
+      <a
+        href="#"
+        className="fs-preload absolute bottom-[100px] left-1/2 flex w-[212px] -translate-x-1/2 justify-center bg-light-black px-6 py-3 font-family-secondary text-[20px] text-main"
+      >
+        Перейти
+      </a>
+    </section>
   );
 };
 
---
+++ src/types/Product.ts
@@ -0,0 +1,21 @@
+import type { Gender } from './Gender';
+
+export interface Product {
+  id: string;
+  name: string;
+  gender: Gender;
+  category:
+    | 'dresses'
+    | 'shirts'
+    | 'summer'
+    | 'pants'
+    | 'outerwear'
+    | 'sweatshirts';
+  price: number;
+  oldPrice: number | null;
+  hasDiscount: boolean;
+  image: string;
+  sizes: string[];
+  color: string;
+  season: 'spring_summer' | 'autumn_winter';
+}
