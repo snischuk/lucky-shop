f87925f24121a27687418b46e273c1743bbb6f5f
Snischuk Maxim <m.snischuk@gmail.com>
2025-06-15 12:52:17 +0300

    refactor: add hook for categories

--- src/layouts/ProductsLayout.tsx
+++ src/layouts/ProductsLayout.tsx
@@ -1,12 +1,20 @@
 import type { FC } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 
-// import { ProductsCategories } from '../components/products/ProductsCategories';
+import { ProductsCategories } from '../components/products/ProductsCategories';
+import { PATH_PAGES } from '../constants/pathPages';
+import { useGenderCategories } from '../hooks/useGenderCategories';
 
 const ProductsLayout: FC = () => {
+  const { gender, categories } = useGenderCategories();
+
+  if (!gender) {
+    return <Navigate to={PATH_PAGES.NOT_FOUND} replace />;
+  }
+
   return (
     <>
-      {/* <ProductsCategories /> */}
+      <ProductsCategories categories={categories} gender={gender} />
       <Outlet />
     </>
   );
