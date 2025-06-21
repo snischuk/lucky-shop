5295f9b2f9bb2518ccc903b29747bacd9e62ada4
halyna <galinabandurka7@gmail.com>
2025-06-21 19:26:52 +0200

    fix: mockProducts not defined

--- src/components/products/ProductsList.tsx
+++ src/components/products/ProductsList.tsx
@@ -1,7 +1,33 @@
 import type { FC } from 'react';
+interface ProductsListProps {
+  gender: 'men' | 'women';
+}
+const mockProducts = [
+  {
+    id: '1',
+    title: 'suknia',
+    price: '1299',
+    image: '',
+  },
+];
 
-const ProductsList: FC = () => {
-  return <div>ProductsList</div>;
+const ProductsList: FC<ProductsListProps> = ({ gender }) => {
+  return (
+    <div className="grid grid-cols-2 gap-6 md:grid-cols-4">
+      {mockProducts.map((product) => (
+        <div key={product.id} className="border p-4 text-center">
+          <img
+            scr={product.image}
+            alt={product.title}
+            className="h-auto w-full"
+          />
+          <h3 className="mt-2 text-lg font-semibold">{product.title}</h3>
+          <p className="text-gray-600">{product.price} грн</p>
+        </div>
+      ))}
+      ProductsList for {gender}
+    </div>
+  );
 };
 
 export { ProductsList };
--- src/pages/products/ProductDetailPage.tsx
+++ src/pages/products/ProductDetailPage.tsx
@@ -2,11 +2,16 @@ import type { FC } from 'react';
 import { useParams } from 'react-router-dom';
 
 const ProductDetailPage: FC = () => {
-  const { productId } = useParams();
+  const { gender, productId } = useParams<{
+    gender: 'men' | 'women';
+    productId: string;
+  }>();
 
   return (
     <div>
-      <h1>Деталі продукту: {productId}</h1>
+      <h1>
+        Деталі продукту: {productId} for {gender}
+      </h1>
       {/* {<ProductDetail/>} */}
       {/* {<ProductsRecommendations/>} */}
     </div>
